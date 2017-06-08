11936c612481f1b7f595fd2631bb8336ecab72ec
marciotoshio <contato@marciotoshio.com.br>
2017-06-08 00:49:51 -0300

    using webpack

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,17 +1,22 @@
-pip install awscli
+#pip install awscli
+#npm install webpack
+npm install
+webpack
+
 # Preparing and deploying Function to Lambda
-zip -r family-facts.zip index.js package.json
-aws lambda update-function-code --function-name familyFacts --zip-file fileb://family-facts.zip
+zip -r family-facts.zip index.js
+
+#aws lambda update-function-code --function-name familyFacts --zip-file fileb://family-facts.zip
 
 # Publishing a new Version of the Lambda function
-version='aws lambda publish-version --function-name familyFacts | jq -r .Version'
+#version=`aws lambda publish-version --function-name familyFacts | jq -r .Version`
 
 # Updating the PROD Lambda Alias so it points to the new function
-aws lambda update-alias --function-name familyFacts --function-version $version --name PROD
+#aws lambda update-alias --function-name familyFacts --function-version $version --name PROD
 
-aws lambda get-function --function-name "familyFacts"
+#aws lambda get-function --function-name "familyFacts"
 
 # Invoking Lambda function from update PROD alias
-aws lambda invoke --function-name familyFacts --payload "$(cat test_data.json)" --qualifier PROD lambda_output.txt
+#aws lambda invoke --function-name familyFacts --payload "$(cat test_data.json)" --qualifier PROD lambda_output.txt
 
-cat lambda_output.txt
+#cat lambda_output.txt
---
+++ webpack.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  entry: { 'app': './app.js' },
+  output: { filename: 'index.js' }
+}
